001e8095139c22bd92e22478357018d5cfb86b7b
Kristina Taing <kataing12@gmail.com>
2019-06-28 11:47:24 -0700

    [prog] built out schema for database

--- database/index.js
+++ database/index.js
@@ -3,14 +3,33 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  name: 'string',
+  login: 'string',
+  full_name: 'string',
+  repos_url: 'string',
+  stargazers_count: 'Number'
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = ((err, model) =) => {
   // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+  // This function should save a repo or repos to the MongoDB
+
+  // console.log('in the save function');
+  // cb(null);
 }
+
+// small.save(function (err) {
+//   if (err) return handleError(err);
+//   // saved!
+// });
+
+// // I added this in
+// let db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+// });
 
 module.exports.save = save;
