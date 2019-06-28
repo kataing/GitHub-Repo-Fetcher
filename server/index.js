ebde3538104cbf0179427545cef0c9acb1bb3805
Kristina Taing <kataing12@gmail.com>
2019-06-28 14:48:05 -0700

    [prog] built out getReposByUsername and save functions

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+
+const parse = require('body-parser');
+const github = require('../helpers/github.js');
+const db = require('../database/index.js');
+
 let app = express();
 
+app.use(parse.json());
+app.use(parse.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos', function (req, res) {
+  const { username } = req.body;
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  github.getReposByUsername(username, (err, { body }) => {
+    db.save(username);
+    res.status(200).send(body);
+  });
+
+  // res.status(201).send('the client post reached the server!');
 });
 
 app.get('/repos', function (req, res) {
